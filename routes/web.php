ab9dd43ed89ab2ee177b2580e16adf8225c563e5
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2019-06-26 16:52:07 +0100

    another commit

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-// Route::get('{path}', 'HomeController@index')->where( 'path','([A-z\d\-\/_.]+)?' );
+Route::get('{path}', 'HomeController@index')->where( 'path','([A-z\d\-\/_.]+)?' );
 
-Route::get('/{any}', 'HomeController@index')->where('any', '.*' );
+// Route::get('/{any}', 'HomeController@index')->where('any', '.*' );
