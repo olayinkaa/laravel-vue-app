32dda1b4e7787199996902f76bc067c25043c324
Ibrahim Olayinka <ibrahimolayinkaa@gmail.com>
2019-07-03 20:37:26 +0100

    another commit

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
     {
         // $this->authorize('isAdmin');
         if (\Gate::allows('isAdmin') || \Gate::allows('isAuthor')) {
-            return User::latest()->paginate(2);
+            return User::latest()->paginate(15);
         }
     }
 
